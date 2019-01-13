c46776ff2a57628064c226a288d5638d3436aaaa
Chris Anderton <cga@heroku.com>
2019-01-13 23:44:22 +0000

    add sonar sh

---
+++ sonar/sonar.sh
@@ -0,0 +1,1 @@
+/app/sonar/bin/sonar-scanner -Dsonar.exclusions=\"/app/sonar/**, /app/vendor/**\" -Dsonar.projectKey=$SONAR_PROJECT_KEY -Dsonar.sources=/app -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_LOGIN
